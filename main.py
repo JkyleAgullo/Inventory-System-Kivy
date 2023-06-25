bc084660896e3e9072ec0b75430bdeabb894b069
matthewanavale <navalematthew12@gmail.com>
2023-06-26 01:06:52 +0800

    latest, showed how to create multiple layouts in a single .kv file and how to call them

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-
+import kivy.utils
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.app import App
+from kivy.core.window import Window
 
 
 
 class MyGrid(Widget):
     pass
 
+class Trylang(Widget):
+    pass
 
-#        self.cols = 2
-#        self.add_widget(Label(text="product name: "))
-#        self.name = TextInput(multiline=False)
-#        self.add_widget(self.name)
 
 
 class MyApp(App):
+    def build(self):
+        Window.clearcolor = (1,1,1,1)
+        return MyGrid()
+
+class Try(App):
     def build(self):
         return MyGrid()
 
 
 
-MyApp().run()
+#MyApp().run()
+Try().run()
