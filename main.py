2986fa0878e65a8fb252a5930ab5b5a91823d229
matthewanavale <navalematthew12@gmail.com>
2023-06-26 22:48:15 +0800

    update

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 kv = Builder.load_file("my.kv")
 
+class MainWindow(Screen):
+    pass
 
+class SecondWindow(Screen):
+    pass
 
-
+class WindowManager(ScreenManager):
+    pass
 
 
 class MyApp(App):
     def build(self):
         Window.clearcolor = (1,1,1,1)
+        Window.size=(1080,720)
         self.title='Inventory_Admin'
         return kv
 
