cf2400fd118b9a15d348a2ed73e4fb83172cb723
matthewanavale <navalematthew12@gmail.com>
2023-06-30 14:04:44 +0800

    changing screens with no transition

--- main.py
+++ main.py
@@ -7,29 +7,34 @@ from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-Builder.load_file("admin.kv")
 
-class AdminDB(Widget):
+
+
+class AdminDB(Screen):
+    pass
+    """
     text_input_string = StringProperty("try")
     qty_input_string = StringProperty("1")
     #insert the string property of the price of the product for printing
+    
     def text_validate(self, widget):
         self.text_input_string = widget.text
     def qty_validate(self, widget):
         self.qty_input_string = widget.text
+        """
 
-
-
-
-class AdminADD(Widget):
+class AdminADD(Screen):
+    pass
+class WindowManager(ScreenManager):
     pass
 
+Builder.load_file('screen.kv')
 class MyApp(App):
     def build(self):
-        Window.clearcolor = (1,1,1,1)
-        Window.size=(1080,720)
-        self.title='Inventory_Admin'
-        return AdminDB()
+        Window.clearcolor = (1, 1, 1, 1)
+        Window.size = (1080, 720)
+        self.title = 'Inventory Admin'
+        return WindowManager()
     #    return AdminADD()
 
 
