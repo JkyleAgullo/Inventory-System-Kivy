288d00ec8c6b795655c64b6c04fe98c1ff91443d
matthewanavale <navalematthew12@gmail.com>
2023-07-02 22:44:05 +0800

    merged the login

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
-
-
-
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.clock import Clock
+from kivy.uix.progressbar import ProgressBar
 
 
 class AdminDB(Screen):
@@ -19,11 +18,21 @@ class AdminDB(Screen):
 
     def text_validate(self, widget):
         self.text_input_string = widget.text
+
     def qty_validate(self, widget):
         self.qty_input_string = widget.text
 
 
 class SplashWindow(Screen):
+    def update_progress(self, value):
+        self.ids.progress_bar.value = value
+        if value >= 100:
+            app = App.get_running_app()
+            app.root.transition = NoTransition()
+            app.root.current = "login"
+
+
+class LoginWindow(Screen):
     pass
 
 
@@ -39,7 +48,7 @@ class AdminSettings(Screen):
 class WindowManager(ScreenManager):
     pass
 
-Builder.load_file('sidePanel.kv')
+Builder.load_file('screen.kv')
 #Builder.load_file('Mainpanel.kv')
 
 class MyApp(App):
@@ -50,8 +59,19 @@ class MyApp(App):
         return WindowManager()
     #    return AdminADD()
 
+    def on_start(self):
+        # Schedule the progress bar updates
+        Clock.schedule_interval(self.update_progress_bar, 0.1)
+
+    def update_progress_bar(self, dt):
+        # Increment the progress bar value
+        current_value = self.root.get_screen('splash').ids.progress_bar.value
+        new_value = current_value + 1
+        self.root.get_screen('splash').update_progress(new_value)
+        if new_value >= 100:
+            # Stop the progress bar updates when the value reaches 100
+            Clock.unschedule(self.update_progress_bar)
 
 
-
-MyApp().run()
-#Try().run()
+if __name__ == "__main__":
+    MyApp().run()
