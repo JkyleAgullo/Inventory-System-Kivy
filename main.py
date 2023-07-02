8daee4814d9dbfd0a9815ba7470a6d41ce66d8bd
JkyleAgullo <jkyleagullo@gmail.com>
2023-07-02 20:39:00 +0800

    added splash and login windows. created a new directory for images.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from kivy.properties import StringProperty
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.clock import Clock
+from kivy.uix.progressbar import ProgressBar
 
 
 class AdminDB(Screen):
@@ -21,6 +23,15 @@ class AdminDB(Screen):
 
 
 class SplashWindow(Screen):
+    def update_progress(self, value):
+        self.ids.progress_bar.value = value
+        if value >= 100:
+            app = App.get_running_app()
+            app.root.transition = NoTransition()
+            app.root.current = "login"
+
+
+class LoginWindow(Screen):
     pass
 
 
@@ -32,17 +43,28 @@ class WindowManager(ScreenManager):
     pass
 
 
-Builder.load_file('screen.kv')
+kv = Builder.load_file("screen.kv")
 
 
 class MyApp(App):
     def build(self):
         Window.clearcolor = (1, 1, 1, 1)
         Window.size = (1080, 720)
-        self.title = 'Inventory Admin'
-        return WindowManager()
-    #    return AdminADD()
+        return kv
+
+    def on_start(self):
+        # Schedule the progress bar updates
+        Clock.schedule_interval(self.update_progress_bar, 0.1)
+
+    def update_progress_bar(self, dt):
+        # Increment the progress bar value
+        current_value = self.root.get_screen('splash').ids.progress_bar.value
+        new_value = current_value + 1
+        self.root.get_screen('splash').update_progress(new_value)
+        if new_value >= 100:
+            # Stop the progress bar updates when the value reaches 100
+            Clock.unschedule(self.update_progress_bar)
 
 
-MyApp().run()
-# Try().run()
+if __name__ == "__main__":
+    MyApp().run()
